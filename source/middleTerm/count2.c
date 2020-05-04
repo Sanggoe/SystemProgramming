d1445d9d4656e1497291f8ba4b2a5be8d61874bc
Sanggoe <sanggoe0509@gmail.com>
2020-05-04 10:42:43 +0900

    middle term assignment

--- source/middleTerm/count2.c
+++ source/middleTerm/count2.c
@@ -2,12 +2,14 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 
 // to show information
 void informMessage() {
     printf("명령어 형식을 확인해주세요.\n");
     printf("count [file_name] : show file's bytes\n");
     printf("count [-s] [file_name] : show contents per 3 lines\n");
+    exit(1);
 }
 
 // to count file's bytes
@@ -35,15 +37,15 @@ int countFileBytes(char* fileName) {
 int main(int argc, char** argv) {
     int fd, len;
     char* buf;
+    struct winsize max;
+    ioctl(0, TIOCGWINSZ, &max); // get window size
 
     switch(argc) {
-	case 1: 
-	    printf("Error! You should enter one file name.\n");
-	    break;
 	case 2: 
 	    len = countFileBytes(argv[1]);
 	    printf("%s : %d\n", argv[1], len);
 	    break;
+
 	case 3:
 	    if(argv[1][0]=='-' && argv[1][1] == 's') {
 		if((fd = open(argv[2], O_RDONLY)) < 0) {
@@ -59,12 +61,27 @@ int main(int argc, char** argv) {
 		}
 
 		int line = 0;
+		int width = 0;
+
 		for(int i=0; i<len; i++) {
-		    if(buf[i] == '\n' && (++line) == 3) { // check every 3th line.
+		    if(buf[i] == '\n' || (++width) == max.ws_col) {
+    			line++; // check every lines, or consol's max column.
+			width = 0;
+		    }
+
+		    if(line == 3) { // check every 3th line.
 			if(getchar() == 'q') {
 			    break;
 			} else {
 			    line = 0;
+			    width = 0;
+			    if(buf[i] != '\n') {
+			    	if(write(1, &buf[i], 1) < 0) {
+                            	    perror("Write"); // check error
+                            	    exit(1);
+                            	}
+			    }
+
 			}
 		    } else {
 			if(write(1, &buf[i], 1) < 0) {
@@ -74,13 +91,17 @@ int main(int argc, char** argv) {
 		    }
 		}
 		free(buf); // memory free
+
 	    } else {
 		informMessage();
 	    }
 	    break;
+
 	default:
 	    informMessage();
 	    break;
+
     }
+
     return 0;
 }
