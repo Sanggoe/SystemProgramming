6fb469b1994ec04c6676c9e2438cedfbd3a96e8a
Sanggoe <sanggoe0509@gmail.com>
2020-05-19 01:55:37 +0900

    formatting and functionize

--- source/chap8/homework1/processMappingGame.c
+++ source/chap8/homework1/processMappingGame.c
@@ -5,6 +5,36 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+// copy to buf from addr
+void addrToBuf(caddr_t addr, char* buf){
+    int i=2;
+
+    while (addr[i] != '\n' && addr[i] != '\0') {
+	buf[i-2] = addr[i];
+	i++;
+    }
+    buf[i-2] = '\0'; // set the last index to '\0'
+}
+
+// copy to addr from buf
+void bufToAddr(caddr_t addr, char* buf, int value) {
+    int i = 0;
+
+    // int to char before value become 0
+    while (value != 0) {
+	buf[i++] = value % 10 + '0';
+	value /= 10;
+    }
+    i--;
+
+    // buf to addr
+    for(int j=2; i>=0; i--, j++) {
+	addr[j] = buf[i];
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     int fd;
@@ -13,6 +43,7 @@ int main(int argc, char* argv[]) {
     pid_t pid;
     caddr_t addr;
     struct stat statbuf;
+    srand((unsigned)time(NULL));
 
     if (argc != 2) { // check right commend
 	fprintf(stderr, "Usage : %s filename\n", argv[0]);
@@ -47,16 +78,10 @@ int main(int argc, char* argv[]) {
 	case 0: /* child process */
 	    while(1) {
 		if (addr[0] == '1') { // if child turn
-		    int i = 2;
-
-		    while (addr[i] != '\n' && addr[i] != '\0') {
-			buf[i-2] = addr[i];
-			i++;
-		    }
-		    buf[i-2] = '\0';
+		    addrToBuf(addr, buf);
 
 		    randNum = rand() % 3 + 1;
-		    value = atoi(buf) + randNum;
+		    value = atoi(buf) + randNum; // char to int
 		    printf("\t %2d %6d\t(c)\n", value, randNum);
 
 		    if(value > 20) {
@@ -65,18 +90,7 @@ int main(int argc, char* argv[]) {
 			break;
 		    }
 
-		    i = 0;
-		    while (value != 0) {
-			buf[i++] = value % 10 + '0';
-			value /= 10;
-		    }
-		    i--;
-
-		    for(int j=2; i>=0; i--, j++) {
-			addr[j] = buf[i];
-		    }
-
-		    sleep(1);
+		    bufToAddr(addr, buf, value);
 		    addr[0] = '0';
 		} else if (addr[0] == '2') {
 		    printf("child process win!\n");
@@ -87,36 +101,19 @@ int main(int argc, char* argv[]) {
 	default: /* parent process */
 	    while(1) {
 		if (addr[0] == '0') { // if parent turn
-		    int i = 2;
-                    
-		    while (addr[i] != '\n' && addr[i] != '\0') {
-			buf[i-2] = addr[i];
-			i++;
-		    }
-                    buf[i-2] = '\0';
-
+		    addrToBuf(addr, buf);
+		    
 		    randNum = rand() % 3 + 1;
                     value = atoi(buf) + randNum;
                     printf("%4d %6d \t\t(p)\n", randNum, value);
 
 		    if(value > 20) {
                         printf("parent process loss!\n");
-                        addr[0] = '2';
+                       	addr[0] = '2';
                         break;
                     }
         
-		    i = 0;
-                    while(value != 0) {
-			buf[i++] = value %10 + '0';
-			value /= 10;
-		    }
-		    i--;
-
-		    for(int j = 2; i >= 0; i--, j++) {
-			addr[j] = buf[i];
-		    }
-
-                    sleep(1);
+		    bufToAddr(addr, buf, value);
 		    addr[0] = '1';
 		} else if (addr[0] == '2') {
 		    printf("parent process win!\n");
@@ -128,4 +125,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-	
