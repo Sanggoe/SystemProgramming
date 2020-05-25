7ef351d038201bc20118f645fa6a196dd72578b1
Sanggoe <sanggoe0509@gmail.com>
2020-05-26 00:30:48 +0900

    edit

---
+++ source/chap7/ex7_1.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Before SIGCONT Signal to parent.\n");
+    
+
+    kill(getppid(), SIGCONT);
+
+    printf("Before SIGCONT Signal to me.\n");
+
+    kill(getpid(), SIGQUIT);
+
+    printf("After SIGQUIT Signal.\n");
+
+    return 0;
+
+}
